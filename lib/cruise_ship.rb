eb41234df3590a07f079dcb763276d5e35ea7e3b
Nate Croteau <natekcroteau@gmail.com>
2020-11-22 22:05:44 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,6 +6,12 @@
 # suite_e: "Crumpet the Elf"
 # }
 
-def select_winner(passengers)
-  # add the code snippet here!
+def select_winner(passengers)                       #parameter calling to the hash 
+  winner = ""                                       #empty winner object 
+  passengers.each do |suite, name|                  #hash.each with suite and name (key and value combo)
+    if suite == :suite_a && name.start_with?("A")   #if true for suite a AND name(key in hash) starts with A then
+      winner = name                                 #that name is assigned to the winner object
+    end
+  end
+  winner                                             #which is called upon here so that you know 
 end
