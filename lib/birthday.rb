0db1724efffa04a34362b5a856deaef2753ceefc
Nate Croteau <natekcroteau@gmail.com>
2020-11-24 22:48:30 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  birthday_kids.each do |kids_name, age|                                 #.each the hash with kids_name = key and age = variable 
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"    #calling upon the piped key and value for input
   end 
 end
